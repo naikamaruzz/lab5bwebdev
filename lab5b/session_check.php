48841369d8af98a643e7cb0b1b573e6bba500575
Nabiha Kamaruzzaman <naikamaruzz@gmail.com>
2024-12-24 11:48:25 +0800

    lab5bwebdev

---
+++ lab5b/session_check.php
@@ -0,0 +1,12 @@
+<?php
+// Start the session only if it is not already active
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Check if the user is not logged in
+if (!isset($_SESSION['matric'])) {
+    header("Location: login_page.php"); // Redirect to login page
+    exit();
+}
+?>
